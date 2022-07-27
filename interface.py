457ad0322bfef24c5ca18223fbb73997fa51ac45
Robin Sharma <36168325+robins98@users.noreply.github.com>
2022-07-27 15:38:23 +0200

    Updated 27.07

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 from fileinput import hook_compressed
 from pyexpat import features, model
+from requests import request
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,7 +10,8 @@ import plotly.graph_objects as go
 from mpl_toolkits.mplot3d import Axes3D
 from numpy import random
 from datetime import datetime
-
+from streamlit_lottie import st_lottie
+import requests
 
 import pandas as pd
 import numpy as np
@@ -141,95 +143,132 @@ def outcome_plot(home_team, away_team):
   
 
 ####################################################################################
-
 ##                Predicting the 2022/2023 Premier Leauge                         ##
 
 #Step 1 Reading and cleaning the dataset
-print(datetime.now().strftime("%H:%M:%S"), "DBG#6: reading and cleaning the dataset")
-pl_22_23 = pd.read_csv("Pl_22_23.csv")
+#print(datetime.now().strftime("%H:%M:%S"), "DBG#6: reading and cleaning the dataset")
+#pl_22_23 = pd.read_csv("Pl_22_23.csv")
 
-pl_22_23.drop(pl_22_23.columns[[0,1, 3]], axis=1, inplace=True)
+#pl_22_23.drop(pl_22_23.columns[[0,1, 3]], axis=1, inplace=True)
 
-pl_22_23['Date'] = pl_22_23['Date'].astype(str)
+#pl_22_23['Date'] = pl_22_23['Date'].astype(str)
 
 #for i in range(0, len(pl_22_23)):
 #  pl_22_23.Date[i] = pl_22_23.Date[i].split(str(" "), 1)[0]
 
 
 
-pl_22_23["Home Team"] = pl_22_23['Home Team'].replace({'Spurs':'Tottenham', "Man Utd" : "Man United"})
-pl_22_23["Away Team"] = pl_22_23['Away Team'].replace({'Spurs':'Tottenham', "Man Utd" : "Man United"})
+#pl_22_23["Home Team"] = pl_22_23['Home Team'].replace({'Spurs':'Tottenham', "Man Utd" : "Man United"})
+#pl_22_23["Away Team"] = pl_22_23['Away Team'].replace({'Spurs':'Tottenham', "Man Utd" : "Man United"})
 
 
-pl_22_23["Home Team"] = pl_22_23['Home Team'].replace({'Nottingham Forest':'Cardiff'})
-pl_22_23["Away Team"] = pl_22_23['Away Team'].replace({'Nottingham Forest':'Cardiff'})
+#pl_22_23["Home Team"] = pl_22_23['Home Team'].replace({'Nottingham Forest':'Cardiff'})
+#pl_22_23["Away Team"] = pl_22_23['Away Team'].replace({'Nottingham Forest':'Cardiff'})
 
 #for i in range(0, 1000):
 #  pl_22_23["Sim_" +str(i) + "_HG"] = 0
 #  pl_22_23["Sim_" + str(i) + "_AG"] = 0
 
 
-a = pd.DataFrame([range(0, 2000)])
+@st.cache
+def test():
+    print(datetime.now().strftime("%H:%M:%S"), "DBG#6: reading and cleaning the dataset")
+    pl_22_23 = pd.read_csv("Pl_22_23.csv")
 
+    pl_22_23.drop(pl_22_23.columns[[0,1, 3]], axis=1, inplace=True)
 
-pl_22_23 = pd.concat([pl_22_23, a], axis = 1)
+    pl_22_23['Date'] = pl_22_23['Date'].astype(str)
 
-
-for j in range(0,1000):
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#7: inside for loop (0,1000)")
-    attack_data_subset = attack_data.iloc[j, :]
-    defence_data_subset = defence_data.iloc[j, :]
-    
-    attack_data_home = attack_data_subset[pl_22_23["Home Team"]].values
-    attack_data_away = attack_data_subset[pl_22_23["Away Team"]].values
-    
-    defence_data_home = defence_data_subset[pl_22_23["Home Team"]].values
-    defence_data_away = defence_data_subset[pl_22_23["Away Team"]].values
-    
-    home_goals = np.exp(np.mean(home) + attack_data_home + defence_data_away)
-    away_goals = np.exp(attack_data_away + defence_data_home)
-    
-    pl_22_23.iloc[:, 4 + 2*j] = random.poisson(home_goals, len(home_goals))
-    pl_22_23.iloc[:, 4 + 2*j + 1] = random.poisson(away_goals, len(away_goals))
+    #for i in range(0, len(pl_22_23)):
+    #  pl_22_23.Date[i] = pl_22_23.Date[i].split(str(" "), 1)[0]
 
 
 
-def Sim_outcome2(sim_number):
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#8: inside Sim_outcome2 function")
-    points = pl_22_23.iloc[:, 1:4]
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.1")
-    x = pl_22_23.iloc[:, (4 + 2*sim_number)]
-    y = pl_22_23.iloc[:, (4 + 2 * sim_number + 1)]
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.2")
+    pl_22_23["Home Team"] = pl_22_23['Home Team'].replace({'Spurs':'Tottenham', "Man Utd" : "Man United"})
+    pl_22_23["Away Team"] = pl_22_23['Away Team'].replace({'Spurs':'Tottenham', "Man Utd" : "Man United"})
 
-    points["Home Points"] = (x > y) *3 + 1*(x == y)
-    points["Away Points"] = (x < y) * 3 + 1*(x == y)
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.3")
+
+    pl_22_23["Home Team"] = pl_22_23['Home Team'].replace({'Nottingham Forest':'Cardiff'})
+    pl_22_23["Away Team"] = pl_22_23['Away Team'].replace({'Nottingham Forest':'Cardiff'})
+
+    a = pd.DataFrame([range(0, 2000)])
+    pl_22_23 = pd.concat([pl_22_23, a], axis = 1)
+    for j in range(0,1000):
+        print(datetime.now().strftime("%H:%M:%S"), "DBG#7: inside for loop (0,1000)")
+        attack_data_subset = attack_data.iloc[j, :]
+        defence_data_subset = defence_data.iloc[j, :]
+        
+        attack_data_home = attack_data_subset[pl_22_23["Home Team"]].values
+        attack_data_away = attack_data_subset[pl_22_23["Away Team"]].values
+        
+        defence_data_home = defence_data_subset[pl_22_23["Home Team"]].values
+        defence_data_away = defence_data_subset[pl_22_23["Away Team"]].values
+        
+        home_goals = np.exp(np.mean(home) + attack_data_home + defence_data_away)
+        away_goals = np.exp(attack_data_away + defence_data_home)
+        
+        pl_22_23.iloc[:, 4 + 2*j] = random.poisson(home_goals, len(home_goals))
+        pl_22_23.iloc[:, 4 + 2*j + 1] = random.poisson(away_goals, len(away_goals))
+    return pl_22_23
+
+
+pl_22_23 = test()
+
+#def Sim_outcome2(sim_number):
+#    print(datetime.now().strftime("%H:%M:%S"), "DBG#8: inside Sim_outcome2 function")
+#    points = pl_22_23.iloc[:, 1:4]
+#    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.1")
+#    x = pl_22_23.iloc[:, (4 + 2*sim_number)]
+#    y = pl_22_23.iloc[:, (4 + 2 * sim_number + 1)]
+#    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.2")
+
+#    points["Home Points"] = (x > y) *3 + 1*(x == y)
+#    points["Away Points"] = (x < y) * 3 + 1*(x == y)
+#    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.3")
     #points.groupby(['Home Team']).sum().iloc[:, 1] #Home Points pr team
     #points.groupby(["Away Team"]).sum().iloc[:, -1] #Away points pr team
 
     #Goals scored pr team
-    goals_scored = (pl_22_23.groupby("Home Team").sum().iloc[:, sim_number*2+ 1].values +pl_22_23.groupby("Away Team").sum().iloc[: , sim_number*2 + 2].values) #Goals scored at away 
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.4")
-    #Goals conceded pr team
-    goals_conceded = (pl_22_23.groupby("Home Team").sum().iloc[:, sim_number*2 + 2].values + pl_22_23.groupby("Away Team").sum().iloc[: , sim_number*2 + 1].values)
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.5")
+#    goals_scored = (pl_22_23.groupby("Home Team").sum().iloc[:, sim_number*2+ 1].values +pl_22_23.groupby("Away Team").sum().iloc[: , sim_number*2 + 2].values) #Goals scored at away 
+#    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.4")
+#    #Goals conceded pr team
+#    goals_conceded = (pl_22_23.groupby("Home Team").sum().iloc[:, sim_number*2 + 2].values + pl_22_23.groupby("Away Team").sum().iloc[: , sim_number*2 + 1].values)
+#    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.5")
+
+#    table1 = pd.DataFrame(points["Home Team"].sort_values(ascending = True).unique(), columns= ["Team"])
+#    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.6")
+#    table1["Games"] = 38
+#    table1["Goals Scored"] = goals_scored
+#    table1["Goals conceded"] = goals_conceded
+#    table1["Goal Difference"] = goals_scored - goals_conceded
+#    table1["Total Points"] = points.groupby(['Away Team']).sum().iloc[: ,-1 ].values + points.groupby(['Home Team']).sum().iloc[:,1].values
+#    table1["Team"] = table1['Team'].replace({'Cardiff':'Nottingham Forest'})
+#    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.7")
+#    return table1
+
+
+def Sim_outcome2(sim_number):
+    points = pl_22_23.iloc[:, 1:4]
+    x = pl_22_23.iloc[:, (4 + 2*sim_number)]
+    y = pl_22_23.iloc[:, (4 + 2 * sim_number + 1)]
+
+    points["Home Points"] = (x > y) *3 + 1*(x == y)
+    points["Away Points"] = (x < y) * 3 + 1*(x == y)
+    points["Home Goals"] = x
+    points["Away Goals"] = y
 
     table1 = pd.DataFrame(points["Home Team"].sort_values(ascending = True).unique(), columns= ["Team"])
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.6")
     table1["Games"] = 38
-    table1["Goals Scored"] = goals_scored
-    table1["Goals conceded"] = goals_conceded
-    table1["Goal Difference"] = goals_scored - goals_conceded
     table1["Total Points"] = points.groupby(['Away Team']).sum().iloc[: ,-1 ].values + points.groupby(['Home Team']).sum().iloc[:,1].values
+    table1["Goal Difference"] = points.groupby("Home Team").sum().iloc[:, 3].values + points.groupby("Away Team").sum().iloc[:, 4].values - points.groupby("Home Team").sum().iloc[:, 4].values - points.groupby("Away Team").sum().iloc[:, 3].values 
     table1["Team"] = table1['Team'].replace({'Cardiff':'Nottingham Forest'})
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#8.7")
+
     return table1
 
 
 
 random.seed(123)
-@st.cache(suppress_st_warning= True)
+#@st.cache
 def simula(id):
     print(datetime.now().strftime("%H:%M:%S"), "DBG#9: inside simula function")
     test = Sim_outcome2(id)[["Team", "Total Points", "Goal Difference"]]
@@ -241,16 +280,15 @@ def simula(id):
 
 
 
-#@st.cache(allow_output_mutation=True)
 #def load_model(model_name):
 #    a = pd.DataFrame([range(0, 1)])
 #    standings = pd.DataFrame(model_name(0))
 #    for i in range(1, 1000):
 #        standings.insert(i, i, model_name(i))
-#    standings.index = standings.index + 1
-    
+#    standings.index = standings.index + 1    
 #    return standings
 
+#@st.cache(allow_output_mutation=True)
 def load_model(func):
     print(datetime.now().strftime("%H:%M:%S"), "DBG#10: inside load_model function")
     test = []
@@ -260,15 +298,23 @@ def load_model(func):
     test = np.reshape(test, (1000, 20)).T
     test = pd.DataFrame(test)
     return test
-
 standings = load_model(simula)
 
+standings.index = standings.index + 1
 
 
 
-index = list(standings.loc[6].value_counts().index)
+random.seed(123)
+#index = list(standings.loc[8].value_counts().index)
+index = ['Man United','Arsenal', 'Chelsea','Leicester','Tottenham','West Ham','Aston Villa', 'Leeds','Brentford',
+ 'Wolves','Crystal Palace','Southampton','Everton','Newcastle','Liverpool','Bournemouth','Brighton','Man City',"Fulham" ,'Nottingham Forest']
+
 Standing_probs = pd.DataFrame(index = index)
+print(standings.index)
 
+print("********")
+print("************* ")
+print("************ ")
 
 for i in range(1, 21):
     print(datetime.now().strftime("%H:%M:%S"), "DBG#11: for loop (1,21)")
@@ -286,9 +332,56 @@ def position_prob(team_name, n):
     return prob
 
 
+def leage_winning_prob():
+    x_bar = list(Standing_probs.index[0:4])
+    x_bar.append("Other")
+    y_axes = list(Standing_probs.iloc[0:4, 0])
+    other = 1 - sum(y_axes)
+    y_axes.append(other)
+    x_pos = np.arange(len(x_bar))
+    fig = plt.figure(figsize=(6,4))    
+    plt.bar(x_bar, y_axes, color=("cyan", "red", "blue", "black", "green"))   
+    # Create names on the x-axis
+    plt.xticks(x_pos, x_bar)
+    plt.title("Probability of winning the leauge")
+    return fig
+
+
+def top4():
+    Top4_prob = pd.DataFrame(index = list(Standing_probs.index))
+    Top4_prob["Top 4 prob"] = Standing_probs.iloc[:, 0].values + Standing_probs.iloc[:, 1].values + Standing_probs.iloc[:, 2].values + Standing_probs.iloc[:, 3].values
+    Top4_prob.sort_values(by = "Top 4 prob", ascending = False, inplace = True)
+    return Top4_prob.head(8)
+
+
+def Europa_leauge():
+    EU_leauge_prob = pd.DataFrame(index = list(Standing_probs.index))
+    EU_leauge_prob["Europa leauge Qualifiers"] = Standing_probs.iloc[:, 4].values + Standing_probs.iloc[:, 5].values
+    EU_leauge_prob.sort_values(by = "Europa leauge Qualifiers", ascending = False, inplace = True)
+    return EU_leauge_prob.head(6)
+
+
+def relegation():
+    # Summar 4: Relegation
+     relegation_prob = pd.DataFrame(index = list(Standing_probs.index))
+     relegation_prob["Relegation prob"] = Standing_probs.iloc[:, 17].values + Standing_probs.iloc[:, 18].values + Standing_probs.iloc[:, 19].values
+     relegation_prob.sort_values(by = "Relegation prob", ascending = False, inplace = True)
+     return relegation_prob.head(6)
+    
+    
+
 ####################################################################################
  
- 
+## Lottie animation
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+lottie_coding = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_V72Aqi.json")
+
  
 
 # #Streamlit
@@ -361,7 +454,6 @@ with score_prediction_model:
 
 
 with Leauge_table_model:
-    print(datetime.now().strftime("%H:%M:%S"), "DBG#17: set league table model properties")
     st.subheader("Leauge Table probability")
     st.text("What is the probability of a given team to end up in n-th position ? ")
 
@@ -372,15 +464,37 @@ with Leauge_table_model:
     display_left_col.subheader("Probability")
     display_left_col.text(position_prob(Team, position))
 
+    with right_col:
+        st_lottie(lottie_coding, height= 300, key = "coding")
+
     
+print(index)
+print(" ")
+print(" ")
+print(" ")
+
 
 with Outcome_model:
     print(datetime.now().strftime("%H:%M:%S"), "DBG#18: set outcome model properties")
     st.subheader("Outcomes for 22/23 season")
 
     options = ["Winning the premier leauge", "Top 4", "Europa leauge (5th, 6th)", "Relegation"]
-    left_col, display_left_col = st.columns(2)
+    left_col, right_col = st.columns(2)
     
     
     Outcome = left_col.selectbox("Select fotball outcome", options= options, index = 0)
-    #f outcome == "Winning the premier"
+    if Outcome == "Winning the premier leauge":
+        left_col.pyplot(leage_winning_prob())
+    if Outcome == "Top 4":
+        left_col.table(top4())
+    
+    if Outcome == "Europa leauge (5th, 6th)":
+        left_col.table(Europa_leauge())
+    
+    if Outcome  == "Relegation":
+        left_col.table(relegation())
+
+    right_col.text("Simulated probabilities of teams finishing in different leauge positions:")
+    right_col.write(Standing_probs)
+
+

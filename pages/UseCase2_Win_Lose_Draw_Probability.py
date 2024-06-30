76fabefa819bb3016d4786b02793ca506e617224
Yulin Tian <tianyulin819@gmail.com>
2024-06-30 10:57:55 +0200

    Update UseCase2_Win_Lose_Draw_Probability.py

--- pages/UseCase2_Win_Lose_Draw_Probability.py
+++ pages/UseCase2_Win_Lose_Draw_Probability.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+def main():
+    st.header('Use Case 2: Win-Lose-Draw Probability')
+    st.write('Win-Lose-Draw Probability Description...')
+
+if __name__ == '__main__':
+    main()

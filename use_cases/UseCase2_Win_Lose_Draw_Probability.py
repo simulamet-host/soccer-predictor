cc837025489efc0dee3b9ba50bdbc7df91282ff3
Yulin Tian <tianyulin819@gmail.com>
2024-06-30 22:05:02 +0200

    Update UseCase2_Win_Lose_Draw_Probability.py

--- use_cases/UseCase2_Win_Lose_Draw_Probability.py
+++ use_cases/UseCase2_Win_Lose_Draw_Probability.py
@@ -1,6 +1,6 @@
 import streamlit as st
-#import matplotlib.pyplot as plt
-#import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
 def show():
     st.header('Probabilities of a team winning a game against a selected team')

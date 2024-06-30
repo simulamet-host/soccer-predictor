8d9f8a666347a0d5b6b5f28ec2f92cd97cb0ba5e
Yulin Tian <tianyulin819@gmail.com>
2024-06-30 11:10:45 +0200

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -1,1 +1,21 @@
+import streamlit as st
+# Create this file in the root directory to handle the multi-page navigation.
 
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run(self):
+        st.sidebar.title('Menu')
+        app = st.sidebar.radio(
+            'Go to',
+            self.apps,
+            format_func=lambda app: app['title'])
+
+        app['function']()

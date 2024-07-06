0ec2d13010e589609a5db4cef5883e9d1ed7fe4c
Yulin Tian <tianyulin819@gmail.com>
2024-07-06 11:18:39 +0200

    Update UseCase1_GoalsScored.py

--- use_cases/UseCase1_GoalsScored.py
+++ use_cases/UseCase1_GoalsScored.py
@@ -41,7 +41,7 @@ def show():
         z = np.random.rand(max_goal_count_away + 1, max_goal_count_home + 1) * 20
         
         ax.bar3d(x.flatten(), y.flatten(), np.zeros_like(z.flatten()), 1, 1, z.flatten(), color='forestgreen')
-        ax.set_xlabel('Goals scored from ' + home_team, labelpad=-5, fontsize=9)
+        ax.set_xlabel('Goals scored from ' + home_team, labelpad=-7, fontsize=9)
         ax.set_ylabel('Goals scored from ' + away_team, labelpad=-5, fontsize=9)
         ax.set_zlabel('Probability score in percentage', labelpad=-3, fontsize=9)
         

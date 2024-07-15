c13ae81c756734f4d80b1875bfeb819f2f8b4437
Cise Midoglu <cise@simula.no>
2024-07-15 12:17:27 +0200

    Create leagues.py

---
+++ utils/leagues.py
@@ -0,0 +1,27 @@
+
+
+league = 'epl'
+
+
+clubs_epl = [...]
+num_league_positions_epl = [...]
+
+clubs_laliga = [...]
+num_league_positions_laliga = [...]
+
+clubs_epl = [...]
+num_league_positions_laliga = [...]
+
+clubs_bundeslina = [...]
+num_league_positions_bundesliga = [...]
+
+clubs_ligue1 = [...]
+num_league_positions_ligue1 = [...]
+
+...
+
+
+function called "get_league_metadata", which takes as input "league" (uses default value from above) and gives as output:
+- "clubs" array
+- "num_league_positions" value
+(for the selected league)

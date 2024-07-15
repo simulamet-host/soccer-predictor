9f13a84ba8248deacec5c1af795feb583a275f28
Cise Midoglu <cise@simula.no>
2024-07-15 12:20:39 +0200

    Update leagues.py

--- utils/leagues.py
+++ utils/leagues.py
@@ -4,19 +4,19 @@ league = 'epl'
 
 
 clubs_epl = [...]
-num_league_positions_epl = [...]
+num_league_positions_epl = 20
 
 clubs_laliga = [...]
-num_league_positions_laliga = [...]
+num_league_positions_laliga = x
 
 clubs_epl = [...]
-num_league_positions_laliga = [...]
+num_league_positions_laliga = y
 
 clubs_bundeslina = [...]
-num_league_positions_bundesliga = [...]
+num_league_positions_bundesliga = z
 
 clubs_ligue1 = [...]
-num_league_positions_ligue1 = [...]
+num_league_positions_ligue1 = w
 
 ...
 

a8c89da42ef4ca36c0a0932c05cd7fc3c506966d
Yulin Tian <tianyulin819@gmail.com>
2024-07-21 12:01:23 +0200

    Update UseCase2_Game_Outcome.py

--- use_cases/UseCase2_Game_Outcome.py
+++ use_cases/UseCase2_Game_Outcome.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 import os
+from leagues import get_league_metadata
 
 # Add the utils directory to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
@@ -14,15 +15,8 @@ def show():
     st.header('Use Case 2: Game Outcome')
     st.write('Game outcome (win-lose-draw) probabilities for selected home and away teams.')
     
-    # List of football clubs
-    clubs = [
-        'Arsenal', 'Aston Villa', 'Blackburn Rovers', 'Chelsea', 'Coventry City', 
-        'Crystal Palace', 'Everton', 'Ipswich Town', 'Leeds United', 'Liverpool', 
-        'Manchester City', 'Manchester United', 'Middlesbrough', 'Norwich City', 
-        'Nottingham Forest', 'Oldham Athletic', 'Queens Park Rangers', 
-        'Sheffield United', 'Sheffield Wednesday', 'Southampton', 'Tottenham Hotspur', 
-        'Wimbledon'
-    ]
+    # Get league metadata
+    clubs, num_league_positions = get_league_metadata('epl')
     
     # Dropdowns for selecting home and away teams
     home_team = st.selectbox('Please select Home Team:', clubs)
